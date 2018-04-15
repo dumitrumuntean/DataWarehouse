054d329b1a548a056bb58800a428f22770851f55
Dumitru Muntean <261824@via.dk>
2018-04-15 10:57:18 +0200

    Birthday working

--- extractOperation.sql
+++ extractOperation.sql
@@ -18,7 +18,8 @@ select    MEMBERNO,
           'NEW' 
           from 
           (
-            select * from taMember where memberno not in (select memberno from YESTERDAYTAMEMBER)
+            select * from taMember 
+            where memberno not in (select memberno from YESTERDAYTAMEMBER)
           );
 
 --deleted = yesterday PK - today PK   
@@ -72,7 +73,38 @@ select    MEMBERNO,
             where memberno not in (select memberno from CHANGEDTAMEMBER 
             where operation='NEW'-- minus new
           ));
+--changed birthday = rows today - rows yesterday - new elements
+insert into CHANGEDTAMEMBER
+select    MEMBERNO,
+          INITIALS,
+          NAME  ,
+          ADDRESS,
+          ZIPCODE,
+          DATEBORN,
+          DATEJOINED,
+          DATELEFT,
+          OWNSPLANEREG,
+          STATUSSTUDENT,
+          STATUSPILOT,
+          STATUSASCAT,
+          STATUSFULLCAT,
+          SEX,
+          CLUB,
+          'CH' 
+          from 
+          (
+            select * from (
+            select * from taMember                --rows today
+            minus                                 --minus
+            select * from YESTERDAYTAMEMBER)    -- rows yesterday
+            where 
+            to_number(to_char(DATEBORN, 'MMDD'))=to_number(to_char(sysdate, 'MMDD')) 
+            and
+             memberno not in (select memberno from CHANGEDTAMEMBER 
+            where operation='NEW' or operation = 'CH'-- minus new
+          )); 
           
+      
     select * from CHANGEDTAMEMBER;
    select count(*) from (select memberno from taMember 
           minus 
@@ -84,7 +116,7 @@ truncate table CHANGEDTAMEMBER;
 
 
 Delete from taMember where memberno='10000';
-insert into taMember (MemberNo, Initials, name, address, zipcode, dateBorn, dateJoined, dateLeft, ownsPlaneReg, statusStudent, statusPilot, statusAsCat, statusFullCat, sex, club ) values(       10000 , 'Dima' , 'Marius' , 'Antonigade, Odense V' ,  5200 , to_date('1965-09-29','YYYY-MM-DD') , to_date('2016-02-05','YYYY-MM-DD') , null , '   ' , 'N' , 'Y' , 'N' , 'N' , 'M' , 'SG70' );
+insert into taMember (MemberNo, Initials, name, address, zipcode, dateBorn, dateJoined, dateLeft, ownsPlaneReg, statusStudent, statusPilot, statusAsCat, statusFullCat, sex, club ) values(       10001 , 'Dima' , 'Marius' , 'Antonigade, Odense V' ,  5200 , to_date('1965-04-15','YYYY-MM-DD') , to_date('2016-02-05','YYYY-MM-DD') , null , '   ' , 'N' , 'Y' , 'N' , 'N' , 'M' , 'SG70' );
 
 update taMember set address='greg' where memberno='231';
 update taMember set address='greg' where memberno='10000';
