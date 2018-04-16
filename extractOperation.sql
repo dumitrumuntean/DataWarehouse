6de573f0e44514327d145546e46bf6ca21c6e5b5
Dumitru Muntean <261824@via.dk>
2018-04-16 17:13:00 +0200

    Not semnificative changes

--- extractOperation.sql
+++ extractOperation.sql
@@ -1,3 +1,5 @@
+-----------------Retrieving new, deleted, changed member------------------------
+
 -- new = today PK - yesterday PK
 insert into CHANGEDTAMEMBER
 select    MEMBERNO,
@@ -101,34 +103,129 @@ select    MEMBERNO,
             to_number(to_char(DATEBORN, 'MMDD'))=to_number(to_char(sysdate, 'MMDD')) 
             and
              memberno not in (select memberno from CHANGEDTAMEMBER 
-            where operation='NEW' or operation = 'CH'-- minus new
+            where operation='NEW' or operation = 'CH'-- minus new and changed
           )); 
-          
-      
-    select * from CHANGEDTAMEMBER;
-   select count(*) from (select memberno from taMember 
-          minus 
-          select memberno from CHANGEDTAMEMBER);
-truncate table YESTERDAYTAMEMBER;
+-------------------Retrieving new, deleted, changed clubs-----------------------
+insert into CHANGEDTACLUB
+select MANE,
+       ADDRESS,
+       ZIPCODE,
+       REGION,
+       'NEW'
+       from(
+       select * from TACLUB
+       where MANE not in(select MANE from YESTERDAYTACLUB)
+       )
+       union all
+select MANE,
+       ADDRESS,
+       ZIPCODE,
+       REGION,
+       'DEL'
+       from(
+       select * from YESTERDAYTACLUB
+       where MANE not in (select MANE from TACLUB)
+       );
+insert into CHANGEDTACLUB
+select MANE,
+       ADDRESS,
+       ZIPCODE,
+       REGION,
+       'DEL'
+       from(select * from(
+       select * from TACLUB
+       minus 
+       select * from YESTERDAYTACLUB)
+       where mane not in(select mane from CHANGEDTACLUB where 
+       OPERATION='NEW')
+       );
+
+----------------------Retrieving new, deleted Region----------------------------
+insert into CHANGEDTAREGION
+select NAME,
+      'NEW'
+      from(
+        select * from TAREGION
+        minus
+        select * from YESTERDAYTAREGION
+      )
+      union all
+select NAME,
+      'DEL'
+      from(
+        select * from YESTERDAYTAREGION
+        minus
+        select * from TAREGION
+      );
+
+---------------Retrieving new from TAFLIGHTSVEJLE--------------------------------
+insert into CHANGEDTAFLIGHTSVEJLE
+select  LAUNCHTIME
+       ,LANDINGTIME
+       ,PLANEREGISTRATION
+       ,PILOT1INIT
+       ,PILOT2INIT
+       ,LAUNCHAEROTOW
+       ,LAUNCHWINCH
+       ,LAUNCHSELFLAUNCH
+       ,CABLEBREAK
+       ,CROSSCOUNTRYKM
+       ,'NEW'
+        from(
+        select * from TAFLIGHTSVEJLE
+        where (LaunchTime, LandingTime, PlaneRegistration) not in (
+        select LaunchTime, LandingTime, PlaneRegistration 
+        from YESTERDAYTAFLIGHTSVEJLE)
+        )
+        UNION ALL
+select  LAUNCHTIME
+       ,LANDINGTIME
+       ,PLANEREGISTRATION
+       ,PILOT1INIT
+       ,PILOT2INIT
+       ,LAUNCHAEROTOW
+       ,LAUNCHWINCH
+       ,LAUNCHSELFLAUNCH
+       ,CABLEBREAK
+       ,CROSSCOUNTRYKM
+       ,'DEL'
+        from(
+        select * from YESTERDAYTAFLIGHTSVEJLE
+        where (LaunchTime, LandingTime, PlaneRegistration) not in (
+        select LaunchTime, LandingTime, PlaneRegistration 
+        from TAFLIGHTSVEJLE)
+        );
+
+--copying from today table into yesterday table
+--it should be done after new, changed and deleted rows are found
 insert into YESTERDAYTAMEMBER
 select * from taMember;
+
+
+-----------------------------------trucate
+truncate table YESTERDAYTAMEMBER;
 truncate table CHANGEDTAMEMBER;
+truncate table CHANGEDTAFLIGHTSVEJLE;
 
-
+---------------------------------add and remove information for test
 Delete from taMember where memberno='10000';
+Delete from taMember where memberno='10001';
+
+insert into taMember (MemberNo, Initials, name, address, zipcode, dateBorn, dateJoined, dateLeft, ownsPlaneReg, statusStudent, statusPilot, statusAsCat, statusFullCat, sex, club ) values(       10000 , 'Dima' , 'Marius' , 'Antonigade, Odense V' ,  5200 , to_date('1965-06-15','YYYY-MM-DD') , to_date('2016-02-05','YYYY-MM-DD') , null , '   ' , 'N' , 'Y' , 'N' , 'N' , 'M' , 'SG70' );
 insert into taMember (MemberNo, Initials, name, address, zipcode, dateBorn, dateJoined, dateLeft, ownsPlaneReg, statusStudent, statusPilot, statusAsCat, statusFullCat, sex, club ) values(       10001 , 'Dima' , 'Marius' , 'Antonigade, Odense V' ,  5200 , to_date('1965-04-15','YYYY-MM-DD') , to_date('2016-02-05','YYYY-MM-DD') , null , '   ' , 'N' , 'Y' , 'N' , 'N' , 'M' , 'SG70' );
 
 update taMember set address='greg' where memberno='231';
 update taMember set address='greg' where memberno='10000';
 
-            select * from (
-            select * from taMember
-            minus
-            select * from YESTERDAYTAMEMBER)
-            where memberno not in (select memberno from CHANGEDTAMEMBER where operation='NEW') ;
-
+---------------------------selects
 select * from YESTERDAYTAMEMBER;
-
-truncate table YESTERDAYTAMEMBER;
-
 select * from CHANGEDTAMEMBER;
+
+---------GETING all columns name------------------
+select case column_id
+        when 1 then 'select  ' || column_name
+        else        '       ,' || column_name
+        end
+from dba_tab_cols
+where owner='DWH'
+and table_name='TAFLIGHTSVEJLE';
